f31294145ac87d297bfd5b6155810d7205b7d795
Raja Sekhar <krajasekhar015@gmail.com>
2019-06-15 09:30:48 +0530

    LinkedList

--- src/main/java/ArrayList/Data.java
+++ src/main/java/ArrayList/Data.java
@@ -15,7 +15,7 @@ public class Data {
             al.add(val);
         }
         System.out.println("==============");
-        return aal;
+        return al;
     }
     public void dataManip(ArrayList al){
         System.out.println("==============");
